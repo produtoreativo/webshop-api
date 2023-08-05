6082a44f149c65ff9738e53553935a88e21d19d9
Christiano Milfont <cmilfont@gmail.com>
2023-08-05 11:08:50 -0300

    estrutura inicial do Service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import OrderDto from './OrderDto';
 
 @Injectable()
 export class AppService {
+  getOrder(id: string) {
+    throw new Error('Method not implemented.');
+  }
+  getProducts() {
+    throw new Error('Method not implemented.');
+  }
+  createOrder(orderDto: OrderDto) {
+    throw new Error('Method not implemented.');
+  }
   getHello(): string {
     return 'Hello World!';
   }
